daf85281b1d1a3802a343ddd72af17ebec97f7fc
Igor Campos <64698809+IgorcamposCODE@users.noreply.github.com>
2022-05-29 21:18:12 -0300

    Adicionando um Println (Imprimir no CMD ou Debugger) Para Executar mais 1 item

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 
     public static void main (String[] args) {
         System.out.println("Olá Mundo");
+        System.out.println("Aqui está meu primeiro programa em Java!");
     }
 }
